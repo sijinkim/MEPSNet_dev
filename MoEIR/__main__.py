2aedda3589afd88b063fa3b68c911905d650788b
YoungbeomChoi <helloybz@gmail.com>
2019-11-27 14:05:40 +0900

    All module skeletons

--- MoEIR/__main__.py
+++ MoEIR/__main__.py
@@ -4,16 +4,18 @@ import torch
 
 from MoEIR.modules.utils import prepare_modules
 
-parser = argparse.ArgumentParser(prog='efef')
+parser = argparse.ArgumentParser(prog='MoEIR')
 parser.add_argument('--feature_extractor', type=str, default='resnet')
-parser.add_argument('--reconstructor', type=str, default='cnn')
-parser.add_argument('--attention', type=str, default='gap')
 parser.add_argument('experts', type=str, nargs='+')
+parser.add_argument('--gate', type=str, default='base')
+parser.add_argument('--reconstructor', type=str, default='base')
+parser.add_argument('--attention', type=str, default='base')
 parser.add_argument('--gpu', type=int, default=None)
 
 opt = parser.parse_args()
 
-device = torch.device('cpu') if not opt.gpu else torch.device(f'cuda:{opt.gpu}')
+device = torch.device('cpu') if not opt.gpu \
+    else torch.device(f'cuda:{opt.gpu}')
 
 # TODO: YB: Prepare module sequence.
 # Module preparation
@@ -21,6 +23,7 @@ module_sequence = prepare_modules(
     module_map={
         'feature_extractor': opt.feature_extractor,
         'experts': opt.experts,
+        'gate': opt.gate,
         'reconstructor': opt.reconstructor,
         'attention': opt.attention,
     },
@@ -28,7 +31,7 @@ module_sequence = prepare_modules(
 )
 
 print(module_sequence)
+
 # TODO: SJ: Load datasets
 
 # TODO: SJ: Train
-
--- MoEIR/modules/feature_extractors.py
+++ MoEIR/modules/feature_extractors.py
@@ -1,13 +1,23 @@
-class ResNet(Module):
+import torch
+from torchvision.models import resnet34
+
+
+class ResNet(torch.nn.Module):
     def __init__(self):
-        from  torchvision.models import resnet152
-        self.resnet = resnet152(pretrained=True)
-        
+        super(ResNet, self).__init__()
+        self.resnet = resnet34(pretrained=True)
+
+        # Drop the last fc layers. B x 512 x 1 x 1
+        self.resnet = torch.nn.Sequential(*list(self.resnet.children())[:-1])
+
         # TODO: Adjust last layers to output 64 dimensions.
 
-        for param in self.resnet.features.parameters():
+        for param in self.resnet.parameters():
             param.requires_grad = False
 
-    def forward(self,x):
-        # 64 x H x W
-        return self.resnet(x)
+    def forward(self, x):
+        return self.resnet(x).squeeze()
+
+    def __repr__(self):
+        return f"{self.__module__.split('.')[-1].upper()} " \
+            f"<{self.__class__.__name__}>"
