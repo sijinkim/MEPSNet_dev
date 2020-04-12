639809901dc8952d034e6f675ee625024de9137d
tlwisdi0306 <tlwisdi0306@gmail.com>
2020-04-12 21:35:40 +0900

    Add CWAinRIR version model training sequence class

--- MoEIR/modules/experts_template.py
+++ MoEIR/modules/experts_template.py
@@ -151,3 +151,87 @@ class MoE_with_Template_without_CWA(nn.Module):
                     'attention': self.attention, 
                     'reconstructor': self.reconstructor}
         return sequence
+
+
+class MoE_with_Template_CWA_in_RIR(nn.Module):
+    def __init__(self, device, n_experts, args):
+        super(MoE_with_Template_CWA_in_RIR, self).__init__()
+        
+        self.n_experts = n_experts
+            
+        # Set feature extraction network        
+        if args.lite_feature:
+            self.feature_extractor = LiteFeatureNet(feature_size=args.featuresize).to(device)
+        elif not args.lite_feature:
+            self.feature_extractor = FeatureNet(feature_size=args.featuresize).to(device)
+        else:
+            print(f"ValueError: Check the argument lite_feature {args.lite_feature}")
+            raise ValueError
+ 
+        # Set reconstruction network
+        if args.lite_reconst:
+            self.reconstructor = LiteReconstructNet(in_channels=args.ex_featuresize, 
+                                                    out_channels=3, 
+                                                    num_experts=n_experts).to(device)
+        elif not args.lite_reconst:
+            self.reconstructor = ReconstructNet(in_channels=args.ex_featuresize, 
+                                                out_channels=3, 
+                                                num_experts=n_experts).to(device)
+        else:
+            print(f"ValueError: Check the argument reconst network {args.lite_reconst}")
+            raise ValueError
+        
+        # Set attention network
+        if args.multi_attention:
+            self.attention = GAP_GMP_AttentionNet(feature_size=args.ex_featuresize,
+                                                  num_experts=n_experts, 
+                                                  gmp_k=args.gmp_k).to(device)
+        elif args.rir_attention:
+            self.attention = AttentionNet_in_RIR(feature_size=args.ex_featuresize, 
+                                                 num_experts=n_experts).to(device)
+
+        elif not args.multi_attention and not args.rir_attention:
+            self.attention = AttentionNet(feature_size=args.ex_featuresize,
+                                          num_experts=n_experts).to(device)
+        else: 
+            print(f"ValueError: Check the argument multi_attention {args.multi_attention}") 
+            raise ValueError        
+   
+        # Set type of experts network
+        ex_type = args.experts[0]
+        if ex_type == 'fvdsr':
+            self.experts = [FVDSRNet(feature_size=args.featuresize, out_feature_size=args.ex_featuresize, kernel_size=args.kernelsize[i]).to(device) for i in range(0, n_experts)]
+        elif ex_type == 'fedsr':
+            self.experts = [FEDSRNet(feature_size=args.featuresize, out_feature_size=args.ex_featuresize, kernel_size=args.kernelsize[i]).to(device) for i in range(0, n_experts)]
+        elif ex_type == 'sfedsr':
+            self.template_bank = SharedTemplateBank(num_bank=args.n_bank, num_templates=args.n_template, first_feature_size=args.ex_featuresize, kernel_size=args.kernelsize[0], device=device).to(device)
+
+            self.experts = [SFEDSRNet(bank=self.template_bank.bank, feature_size=args.featuresize, out_feature_size=args.ex_featuresize, n_resblocks=args.n_resblock, n_templates=args.n_template, device=device, rir=args.rir, rir_cwa=args.rir_attention, sresblocks_in_rir=args.n_sres).to(device) for _ in range(0, n_experts)]
+        else:
+            raise ValueError
+
+
+    def forward(self, x):
+        feature = self.feature_extractor(x)
+        experts_output = [expert(feature) for expert in self.experts]
+        concat_experts = torch.cat(tuple(experts_output), dim=1)
+        cwa_output = self.attention(concat_experts)
+        final_output = self.reconstructor(cwa_output)
+        return final_output
+
+    def forward_valid_phase(self, x):
+        feature = self.feature_extractor(x)
+        experts_output = [expert(feature) for expert in self.experts]
+        concat_experts = torch.cat(tuple(experts_output), dim=1)
+        cwa_output = self.attention(concat_experts)
+        final_output = self.reconstructor(cwa_output)
+        return final_output
+
+    def take_modules(self):
+        sequence = {'feature_extractor': self.feature_extractor, 
+                    'experts': self.experts, 
+                    'attention': self.attention, 
+                    'reconstructor': self.reconstructor}
+        return sequence
+
+
